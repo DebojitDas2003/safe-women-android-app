0472dfc5e8a9fee30242861b26555de280c2ef14
DebojitDas2003 <debojitdas0110@gmail.com>
2024-01-22 01:49:06 +0530

    Initial Demo App

---
+++ app/src/main/java/com/example/safewomen/loginPage.kt
@@ -0,0 +1,61 @@
+package com.example.safewomen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginPage(onLoginSuccess: () -> Unit) {
+    var email by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment  = Alignment.CenterHorizontally,
+        ){
+
+        Text(text = "Email", modifier = Modifier.padding(start = 25.dp))
+        TextField(
+            value = email,
+            onValueChange = {email = it},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp),
+            singleLine = true)
+        Text(text = "Password",
+            modifier = Modifier.padding(start = 25.dp)
+            )
+        TextField(
+            value = password,
+            onValueChange = {password = it},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp)
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(onClick = { /*TODO*/ }){
+            Text(text = "Login")
+        }
+    }
+}
