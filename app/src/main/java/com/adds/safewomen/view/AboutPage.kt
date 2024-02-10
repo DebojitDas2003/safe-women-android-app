4836ca794a12b3cc04498b59a048b9837f234aa6
DebojitDas2003 <debojitdas0110@gmail.com>
2024-02-10 23:17:44 +0530

    Repush-2

--- app/src/main/java/com/adds/safewomen/view/AboutPage.kt
+++ app/src/main/java/com/adds/safewomen/view/AboutPage.kt
@@ -1,5 +1,6 @@
-package com.adds.safewomen.view
+package com.adds.safewomen.view.com.adds.safewomen.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,11 +21,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.safewomen.R
 
 
 @Composable
@@ -35,12 +39,12 @@ fun AboutScreen() {
             .background(MaterialTheme.colorScheme.background)
     ) {
 
-//        Image(
-//            painter = painterResource(id = R.drawable.bg_1),
-//            contentDescription = "",
-//            contentScale = ContentScale.Crop,
-//            modifier = Modifier.fillMaxSize()
-//        )
+        Image(
+            painter = painterResource(id = R.mipmap.ic_launcher_foreground),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
 
         Column(
             modifier = Modifier
--- app/src/main/java/com/adds/safewomen/view/SettingsPage.kt
+++ app/src/main/java/com/adds/safewomen/view/SettingsPage.kt
@@ -8,8 +8,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,8 +15,6 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun SettingsPage() {
-    val incognitoMode = remember { mutableStateOf(true) }
-
     Box(
         modifier = Modifier
             .fillMaxSize()
