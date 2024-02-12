9bc2525a3870cbe5f17d269870de7b9cd977cda9
Debasmita Sarkar <debasmitasarkar@Debasmitas-MacBook-Air.local>
2024-02-12 14:57:10 +0530

    register veiwmodel

--- app/src/main/java/com/adds/safewomen/viewmodel/RegisterPageViewModel.kt
+++ app/src/main/java/com/adds/safewomen/viewmodel/RegisterPageViewModel.kt
@@ -1,4 +1,15 @@
 package com.adds.safewomen.viewmodel
 
-class RegisterPageViewModel {
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+    // Define your ViewModel properties here (e.g., user input fields, validation flags)
+
+    // Example function to handle registration logic
+    fun registerUser(username: String, password: String) {
+        // Implement your registration logic here
+        // You can interact with your data repository or API
+        // For demonstration purposes, let's just print the input
+        println("User registered: Username=$username, Password=$password")
+    }
 }
