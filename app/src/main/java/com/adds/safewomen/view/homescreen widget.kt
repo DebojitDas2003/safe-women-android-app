fbc1999d3a9bb14335d15a12abe914ee8d134e94
debasmitasarkar <debasmitasarkar2003@gmail.com>
2024-02-18 08:49:41 +0530

    hmm

--- app/src/main/java/com/adds/safewomen/view/homescreen widget.kt
+++ app/src/main/java/com/adds/safewomen/view/homescreen widget.kt
@@ -1,18 +1,5 @@
 package com.adds.safewomen.view
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.adds.safewomen.viewmodel.homescreenwidgetViewModel
 
-@Composable
-fun HomeScreenWidget() {
-    val viewModel: homescreenwidgetViewModel = viewModel()
 
-}
 
-@Composable
-@Preview(showBackground = true)
-fun PreviewHomeScreenWidget() {
-    HomeScreenWidget()
-}
--- app/src/main/java/homescreenwidgetViewModel.kt
+++ app/src/main/java/homescreenwidgetViewModel.kt
@@ -1,7 +1,3 @@
 package com.adds.safewomen.viewmodel
 
-import androidx.lifecycle.ViewModel
 
-class homescreenwidgetViewModel : ViewModel() {
-    // You can add your ViewModel logic here if needed
-}
